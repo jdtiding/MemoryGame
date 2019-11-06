a6915f1ba10ec37748ad32f8838da6de76a254d0
jdtiding <jdobkowska1@gmail.com>
2019-11-06 12:32:35 +0100

    Updated line 34 - displayCardsArrayHard changed to displayCardsArrayMedium

--- assets/js/game.js
+++ assets/js/game.js
@@ -31,7 +31,7 @@ $(document).ready(function() {
     clearGrid();
     createDashboardMedium();
     displayCardsArrayMedium = createCardsDeck(activeCardsArray, 32)
-    displayShuffledCards(displayCardsArrayHard);
+    displayShuffledCards(displayCardsArrayMedium);
     $('.card-easy').hide();
     $('.card-hard').hide();
     $('.card-medium').show();
@@ -57,7 +57,7 @@ $(document).ready(function() {
   
   // functions that builds a grid for level medium
   function createDashboardMedium() {
-  let dashboardMedium = $("<div class='card-count card-medium'><div id='force-flip' class='flip-card-inner'><div class='flip-card-front game-card face-down'></div><div class='flip-card-back game-card face-up'></div></div></div>");
+  let dashboardMedium = $("<div class='card-count card-medium'><div class='flip-card-inner'><div class='flip-card-front game-card face-down'></div><div class='flip-card-back game-card face-up'></div></div></div>");
   for (let x = 0; x < 32; x++) {
     dashboardMedium.clone().appendTo('#game');
   }
